85dfe1c8cfb6b6bd6ee3d246bbe2530675b6bbc7
Trqz <joaomarcostroquez@gmail.com>
2023-05-27 22:02:33 +0100

    programming dragable platform

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb2D;
     [SerializeField] private Transform target;
+    [SerializeField] private float speedMultiplier = 16f;
 
     private Vector3 mOffset;
     private float mZCoord;
 
+    private void Start()
+    {
+        target.SetParent(null, true);
+        rb2D.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = ((Vector2)target.transform.position - rb2D.position) * speedMultiplier;
+    }
+
     void OnMouseDown()
     {
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mZCoord = Camera.main.WorldToScreenPoint(target.transform.position).z;
 
         // Store offset = gameobject world pos - mouse world pos
-        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+        mOffset = target.transform.position - GetMouseAsWorldPoint();
 
+        rb2D.bodyType = RigidbodyType2D.Dynamic;
     }
 
     private Vector3 GetMouseAsWorldPoint()
@@ -32,6 +46,15 @@ public class PlatformController : MonoBehaviour
 
     void OnMouseDrag()
     {
-        transform.position = GetMouseAsWorldPoint() + mOffset;
+        target.transform.position = GetMouseAsWorldPoint() + mOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        target.position = transform.position;
+
+        rb2D.velocity = Vector2.zero;
+
+        rb2D.bodyType = RigidbodyType2D.Static;
     }
 }
